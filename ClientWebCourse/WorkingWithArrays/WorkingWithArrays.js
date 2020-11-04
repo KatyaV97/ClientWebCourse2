643384656bfc53763d666fe743493534be9c3d30
Ekaterina Vasileva <ekaterinavasileva01@gmail.com>
2020-11-04 20:16:50 +0700

    Разработала JavaScript для работы с объектами

--- ClientWebCourse/WorkingWithArrays/WorkingWithArrays.js
+++ ClientWebCourse/WorkingWithArrays/WorkingWithArrays.js
@@ -2,18 +2,18 @@
 
 AIS.currentArray1 = [5, 9, 6, - 2, 10, 7, 8, 22];
 
+//1. Сортировка по убыванию
 console.log(AIS.currentArray1.sort(function (arrayValue1, arrayValue2) {
-    return arrayValue1 - arrayValue2;
+    return arrayValue2 - arrayValue1;
 }));
 
-console.log(AIS.currentArray1.filter(function (arrayValue, index) {
-    return index < 5;
-}));
+//2.1. Получение первых пяти элементов
+console.log(AIS.currentArray1.slice(0, 5));
 
-console.log(AIS.currentArray1.filter(function (arrayValue, index) {
-    return index > AIS.currentArray1.length - 6;
-}));
+//2.2. Получение последних пяти элементов
+console.log(AIS.currentArray1.slice(AIS.currentArray1.length - 5));
 
+//3. Получение суммы всех четных чисел массива
 console.log(AIS.currentArray1.filter(function (arrayValue) {
     return arrayValue % 2 === 0;
 }).reduce(function (evenElementsSum, arrayValue) {
@@ -22,12 +22,13 @@ console.log(AIS.currentArray1.filter(function (arrayValue) {
 
 AIS.currentArray2 = [];
 
-for (i = 0; i < 100; i++) {
-    AIS.currentArray2[i] = i + 1;
+for (i = 1; i <= 100; i++) {
+    AIS.currentArray2.push(i);
 }
 
 console.log(AIS.currentArray2);
 
+//4. Получение списка квадратов всех четных чисел массива
 console.log(AIS.currentArray2.filter(function (arrayValue) {
     return arrayValue % 2 === 0;
 }).map(function (arrayValue) {
---
+++ ClientWebCourse/WorkingWithObjects/WorkingWithObjects.js
@@ -0,0 +1,61 @@
+﻿var AIS = AIS || {};
+
+AIS.countries = [AIS.Kanada = {}, AIS.USA = {}, AIS.Mexico = {}];
+
+AIS.Kanada.name = "Kanada";
+AIS.Kanada.cities = [AIS.Ottawa = {}, AIS.Toronto = {}, AIS.Vancouver = {}];
+
+AIS.Ottawa.name = "Ottawa";
+AIS.Ottawa.population = 934243;
+
+AIS.Toronto.name = "Toronto";
+AIS.Toronto.population = 2731571;
+
+AIS.Vancouver.name = "Vancouver";
+AIS.Vancouver.population = 2463431;
+
+AIS.USA.name = "USA";
+AIS.USA.cities = [AIS.Washington = {}, AIS.Chicago = {}, AIS.Houston = {}, AIS.NewYork = {}];
+
+AIS.Washington.name = "Washington";
+AIS.Washington.population = 601723;
+
+AIS.Chicago.name = "Chicago";
+AIS.Chicago.population = 2693976;
+
+AIS.Houston.name = "Houston";
+AIS.Houston.population = 7066141;
+
+AIS.NewYork.name = "New York";
+AIS.NewYork.population = 8405837;
+
+AIS.Mexico.name = "Mexico";
+AIS.Mexico.cities = [AIS.MexicoCity = {}, AIS.Monterrey = {}];
+
+AIS.MexicoCity.name = "Mexico";
+AIS.MexicoCity.population = 8918653;
+
+AIS.Monterrey.name = "Monterey";
+AIS.Monterrey.population = 4689601;
+
+//1. Вывод страны/стран с максимальным количеством городов
+console.log(AIS.countries.sort(function (country1, country2) {
+    return country2.cities.length - country1.cities.length;
+}).filter(function (country) {
+    return country.cities.length === AIS.countries[0].cities.length;
+}));
+
+AIS.CountriesInformation = {};
+
+AIS.countries.forEach(function (country) {
+    AIS.CountriesInformation[country.name] = country.cities.reduce(function (citiesPopulation, city) {
+        return citiesPopulation.population + city.population;
+    });
+});
+
+console.log(AIS.CountriesInformation);
+/*
+for (var i = 0; i < AIS.countries.length; i++) {
+    AIS.CountriesInformation[AIS.countries[i].name] = AIS.countries[i].cities;
+}
+*/
