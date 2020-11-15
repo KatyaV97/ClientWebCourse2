89038c911fbf3821b45296dcc2a6a2541397e9fe
Ekaterina Vasileva <ekaterinavasileva01@gmail.com>
2020-11-15 21:26:57 +0700

    Создала новый проект Temperature

---
+++ ClientWebCourse/Temperature/TemperaturesConverting.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var initialValue = document.getElementById("initial_value").nodeValue;
+
+    var convertButton = document.querySelector("convert_button");
+    
+    convertButton.addEventListener("click", function () {
+        var result = document.createElement("<p>");
+        result.textContent(initialValue);
+        document.body.appendChild(result);
+    })
+})
