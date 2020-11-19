4f2461b9cca47350c7cb0060791f5ef052fcb24a
Ekaterina Vasileva <ekaterinavasileva01@gmail.com>
2020-11-19 08:31:51 +0700

    Исправила замечания от 5.11.20

--- ClientWebCourse/WorkingWithObjects/WorkingWithObjects.js
+++ ClientWebCourse/WorkingWithObjects/WorkingWithObjects.js
@@ -1,65 +1,77 @@
-﻿var AIS = AIS || {};
+﻿(function () {
+    var AIS = AIS || {};
 
-AIS.countries = [AIS.Kanada = {}, AIS.USA = {}, AIS.Mexico = {}];
+    AIS.countries = [AIS.Kanada = {
+        name: "Kanada",
+        cities: [AIS.Ottawa = {
+            name: "Ottawa",
+            population: 934243
+        },
+        AIS.Toronto = {
+            name: "Toronto",
+            population: 2731571
+        },
+        AIS.Vancouver = {
+            name: "Vancouver",
+            population: 2463431
+        }]
+    }, AIS.USA = {
+        name: "USA",
+        cities: [AIS.Washington = {
+            name: "Washington",
+            population: 601723
+        }, AIS.Chicago = {
+            name: "Chicago",
+            population: 2693976
+        },
+        AIS.Houston = {
+            name: "Houston",
+            population: 7066141
+        }, AIS.NewYork = {
+            name: "New York",
+            population: 8405837
+        }]
+    }, AIS.Mexico = {
+        name: "Mexico",
+        cities: [AIS.MexicoCity = {
+            name: "Mexico",
+            population: 8918653
+        }, AIS.Monterrey = {
+            name: "Monterey",
+            population: 4689601
+        }]
+    }];
 
-AIS.Kanada.name = "Kanada";
-AIS.Kanada.cities = [AIS.Ottawa = {}, AIS.Toronto = {}, AIS.Vancouver = {}];
+    // 1. Вывод страны/стран с максимальным количеством городов
+    console.log(getCountriesWithMaxCountCities(AIS.countries));
 
-AIS.Ottawa.name = "Ottawa";
-AIS.Ottawa.population = 934243;
+    AIS.CountriesInformation = {};
 
-AIS.Toronto.name = "Toronto";
-AIS.Toronto.population = 2731571;
-
-AIS.Vancouver.name = "Vancouver";
-AIS.Vancouver.population = 2463431;
-
-AIS.USA.name = "USA";
-AIS.USA.cities = [AIS.Washington = {}, AIS.Chicago = {}, AIS.Houston = {}, AIS.NewYork = {}];
-
-AIS.Washington.name = "Washington";
-AIS.Washington.population = 601723;
-
-AIS.Chicago.name = "Chicago";
-AIS.Chicago.population = 2693976;
-
-AIS.Houston.name = "Houston";
-AIS.Houston.population = 7066141;
-
-AIS.NewYork.name = "New York";
-AIS.NewYork.population = 8405837;
-
-AIS.Mexico.name = "Mexico";
-AIS.Mexico.cities = [AIS.MexicoCity = {}, AIS.Monterrey = {}];
-
-AIS.MexicoCity.name = "Mexico";
-AIS.MexicoCity.population = 8918653;
-
-AIS.Monterrey.name = "Monterey";
-AIS.Monterrey.population = 4689601;
-
-//1. Вывод страны/стран с максимальным количеством городов
-console.log(AIS.countries.sort(function (country1, country2) {
-    return country2.cities.length - country1.cities.length;
-}).filter(function (country) {
-    return country.cities.length === AIS.countries[0].cities.length;
-}));
-
-AIS.CountriesInformation = {};
-
-//2. Создание объекта с информацией о каждой стране
-AIS.countries.forEach(function (country) {
-    AIS.CountriesInformation[country.name] = getCountryPopulation(country);
-});
-
-console.log(AIS.CountriesInformation);
-
-function getCountryPopulation(country) {
-    var population = 0;
-
-    country.cities.forEach(function (city) {
-        population += city.population;
+    // 2. Создание объекта с информацией о каждой стране
+    AIS.countries.forEach(function (country) {
+        AIS.CountriesInformation[country.name] = getCountryPopulation(country.cities);
     });
 
-    return population;
-};
+    console.log(AIS.CountriesInformation);
+
+    function getCountriesWithMaxCountCities(country) {
+        AIS.maxCountCities = 0;
+
+        country.forEach(function (e) {
+            if (e.cities.length > AIS.maxCountCities) {
+                AIS.maxCountCities = e.cities.length;
+            }
+        })
+
+        return country.filter(function (e) {
+            return e.cities.length === AIS.maxCountCities;
+        })
+    }
+
+    function getCountryPopulation(cities) {
+
+        return cities.reduce(function (countryPopulation, e) {
+            return countryPopulation + e.population;
+        }, 0)
+    };
+})();
