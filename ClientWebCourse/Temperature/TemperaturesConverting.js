6fb079c1fa086ac5e041e647adf9a5cf9ad9d7a9
Ekaterina Vasileva <ekaterinavasileva01@gmail.com>
2020-11-22 17:42:43 +0700

    Разработала форму для перевода температур.

--- ClientWebCourse/Temperature/TemperaturesConverting.js
+++ ClientWebCourse/Temperature/TemperaturesConverting.js
@@ -1,11 +1,34 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    var initialValue = document.getElementById("initial_value").nodeValue;
+    var AIS = AIS || {};
 
-    var convertButton = document.querySelector("convert_button");
-    
-    convertButton.addEventListener("click", function () {
-        var result = document.createElement("<p>");
-        result.textContent(initialValue);
-        document.body.appendChild(result);
+    AIS.initialValue = document.getElementById("initial_value");
+    AIS.scale = document.getElementById("scale");
+    AIS.error = document.getElementById("error_message");
+    AIS.result = document.getElementById("result");
+
+    document.getElementById("convert_button").addEventListener("click", function () {
+        AIS.result.textContent = "";
+
+        if (AIS.initialValue.value === "") {
+            AIS.error.textContent = "Введите значение температуры!";
+            return;
+        }
+        else if (AIS.scale.options.selectedIndex === -1) {
+            AIS.error.textContent = "Выберете шкалу перевода!";
+            return;
+        }
+
+        AIS.error.textContent = "";
+
+        AIS.result.textContent = "Преобразованное значение: " + getConvertedTemperature();
     })
+
+    function getConvertedTemperature() {
+        switch (AIS.scale.options[AIS.scale.options.selectedIndex].text) {
+            case "Кельвин":
+                return Number(AIS.initialValue.value) + 273.15;
+            case "Фаренгейт":
+                return Number(AIS.initialValue.value) * 1.8 + 32;
+        }
+    }
 })
